1cb5db9d321ceb7c761b7ca90fefffbfbef531c4
Ingrid Cristina Castro <nocetti.paula@gmail.com>
2020-09-01 12:23:08 -0300

    consumo api, segurança, cadastrar, login

--- src/app/perfil-lateral/perfil-lateral.component.ts
+++ src/app/perfil-lateral/perfil-lateral.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilLateralComponent implements OnInit {
 
+  nome: string = 'Paula Nocetti'
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    
   }
 
 }
